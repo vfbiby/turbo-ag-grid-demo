11dbe027185d58d529eb19f2e9b9dcd1f38be93f
James Zhang <liulfbaby@gmail.com>
2022-12-25 22:23:36 +0800

    feat: add dark mode

--- src/pages/sycm/Sycm.tsx
+++ src/pages/sycm/Sycm.tsx
@@ -2,12 +2,18 @@ import { useCallback, useMemo, useState } from "react";
 import { SycmParser } from "./SycmParser";
 import { AgGridReact } from "ag-grid-react";
 import {
+  Box,
   Button,
+  Container,
+  createTheme,
+  CssBaseline,
   Dialog,
   DialogActions,
   DialogContent,
   DialogTitle,
+  Stack,
   TextareaAutosize,
+  ThemeProvider,
 } from "@mui/material";
 
 export type SycmDataProps = {
@@ -22,6 +28,11 @@ export function Sycm() {
   const [rawData, setRawData] = useState("");
   const [rowData, setRowData] = useState<SycmDataProps[] | null>(null);
   const [isOpen, setIsOpen] = useState(false);
+  const darkTheme = createTheme({
+    palette: {
+      mode: "light",
+    },
+  });
 
   const onParseData = useCallback(() => {
     const parser = new SycmParser();
@@ -41,38 +52,55 @@ export function Sycm() {
   );
 
   return (
-    <div>
-      <div>
-        {isOpen && (
-          <Dialog open={isOpen}>
-            <DialogTitle>Paste data to parse</DialogTitle>
-            <DialogContent>
-              <TextareaAutosize
-                style={{ width: "500px" }}
-                minRows={10}
-                value={rawData}
-                onChange={(e) => setRawData(e.target.value)}
-              />
-            </DialogContent>
-            <DialogActions>
-              <Button variant="contained" onClick={onParseData}>
-                parse
-              </Button>
-            </DialogActions>
-          </Dialog>
-        )}
-      </div>
-      <div>
-        <Button variant="outlined" onClick={() => setIsOpen(true)}>
-          paste
-        </Button>
-        <Button variant="contained" onClick={onParseData}>
-          parse
-        </Button>
-      </div>
-      <div className="ag-theme-material" style={{ height: 400, width: 1000 }}>
-        <AgGridReact columnDefs={colDef} rowData={rowData} />
-      </div>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Container maxWidth="md">
+        <div>
+          {isOpen && (
+            <Dialog open={isOpen}>
+              <DialogTitle>Paste data to parse</DialogTitle>
+              <DialogContent>
+                <TextareaAutosize
+                  style={{ width: "500px" }}
+                  minRows={10}
+                  value={rawData}
+                  onChange={(e) => setRawData(e.target.value)}
+                />
+              </DialogContent>
+              <DialogActions>
+                <Button variant="contained" onClick={onParseData}>
+                  parse
+                </Button>
+              </DialogActions>
+            </Dialog>
+          )}
+        </div>
+
+        <Stack m={1} direction="row" justifyContent="flex-end" spacing={1}>
+          <Button
+            sx={{
+              mx: 1,
+            }}
+            color="warning"
+            variant="outlined"
+            onClick={() => setIsOpen(true)}
+          >
+            paste
+          </Button>
+          <Button variant="contained" onClick={onParseData}>
+            parse
+          </Button>
+        </Stack>
+
+        <Box
+          borderRadius={2}
+          overflow="hidden"
+          className="ag-theme-material"
+          style={{ height: "80vh", width: "100vh" }}
+        >
+          <AgGridReact columnDefs={colDef} rowData={rowData} />
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
